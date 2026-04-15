195709d07b77dcc8699972b7fb4ca7c3c295fb4d
Aleksei Volodin <alexeyw@gmail.com>
2026-04-15 11:46:07 -0400

    chore: remove unused test files `test.kt` and `test_litert.kt`

--- test.kt
+++
@@ -1,3 +0,0 @@
-import com.mikepenz.markdown.m3.markdownTypography
-import com.mikepenz.markdown.m3.Markdown
-fun main() {}
--- test_litert.kt
+++
@@ -1,6 +0,0 @@
-import com.google.ai.edge.litertlm.Backend
-fun test() {
-    val b1 = Backend.CPU()
-    val b2 = Backend.GPU()
-    val b3 = Backend.NPU()
-}
